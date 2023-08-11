b350bd427158c6a58b48d14a80e9d96612581518
Taruun <phantmetic@gmail.com>
2023-08-11 21:42:52 +0530

    solved daily challenge

---
+++ Array/6. Rotate Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def reverseFunction(arr, start, end):
+            while  start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        k = k % len(nums)
+        reverseFunction(nums, 0, len(nums) - 1)
+        reverseFunction(nums, 0, k - 1)
+        reverseFunction(nums, k, len(nums) - 1)
